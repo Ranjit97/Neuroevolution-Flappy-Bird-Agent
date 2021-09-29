71cfb816838d6f8cdd908ae599d86cd8ac67989c
Ranjit97 <karwal.rojen@gmail.com>
2021-09-30 00:10:41 +0530

    Add files via upload

---
+++ pipe.js
@@ -0,0 +1,47 @@
+class Pipe {
+  
+  constructor() {
+//    this.top = random(height/2);
+//    this.bottom = random(height/2);
+    this.top = random(15,height-205);
+    this.bottom = height - this.top - random(90,130)
+    this.x = width;
+    this.w = 30;
+    this.speed = 5;
+    this.highlight = false;
+  }
+  
+  hits(bird) {
+    if(bird.y - 16 < this.top || bird.y + 16 > height - this.bottom) {
+      if(bird.x > this.x && bird.x < this.x + this.w ) {
+        this.highlight = true;
+        return true;
+      }
+    }
+    this.highlight = false;
+    return false;
+    
+  }
+  
+  show() {
+    fill(255);
+    if(this.highlight) {
+      fill(255,0,0);
+    }
+    image(pipe_png, this.x, 0 , this.w, this.top);
+    image(pipe_png, this.x, height - this.bottom , this.w, this.bottom);
+  };
+  
+  update() {
+    this.x -= this.speed;
+  }
+  
+  offscreen() {
+    if(this.x < -this.w) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+}
---
+++ sketch.js
@@ -0,0 +1,73 @@
+let pipes = [];
+let counter = 0;
+let slider;
+var cnv;
+let bird_png;
+let pipe_png;
+let bg;
+
+let brainJSON;
+
+function preload() {
+    brainJSON = loadJSON("best_bird.json");
+}
+
+function setup() {
+  cnv = createCanvas(600,400);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  bird_png = loadImage('bird.png');
+  pipe_png = loadImage('pipe.png');
+  bg = loadImage('background_png.png');
+  input = createP('Slider to speed up');
+  input.position((windowWidth / 2) - 250,(windowHeight/2) + 210 );
+  //speedup = createElement('h2', 'Slider to spped up the training');
+  slider = createSlider(1, 1000, 1);
+  slider.position((windowWidth / 2) - 250,(windowHeight/2) + 250 );
+  let birdBrain = NeuralNetwork.deserialize(brainJSON);
+  bird = new Bird(birdBrain);
+}
+
+function draw() {
+  
+  
+  for(let n = 0; n<slider.value(); n++) {
+    if(counter % 50 == 0) {
+      pipes.push(new Pipe());
+    }
+    
+    counter++;
+    
+    for(var i = pipes.length-1; i>=0; i--) {
+      //pipes[i].show();
+      pipes[i].update();
+      
+    if(pipes[i].hits(bird)) {
+        console.log("collison");
+    }
+      
+    if(pipes[i].offscreen()) {
+        pipes.splice(i,1);
+        }
+    }
+    
+    if(bird.offScreen()) {
+        console.log("bottom");
+    }
+    
+      bird.think(pipes);
+      bird.update();
+    
+  }
+  
+  //All the drawing Stuff here
+  background(bg);
+  
+  bird.show();
+  
+  for(let pipe of pipes) {
+    pipe.show();
+  }
+  
+}
